f40416e32e3c4b92dbfe7144551e4a868f77ae32
Mitish Raina <rainamitish7@gmail.com>
2025-04-20 14:27:39 +0530

    final commit

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig(({ mode }) => ({
     port: 5173,
   },
   plugins: [react()].filter(Boolean),
-  base: "/",
+  base: "/DewDrop",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
